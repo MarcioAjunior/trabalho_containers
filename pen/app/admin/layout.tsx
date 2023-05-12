4214d2be6fe1124948210c01486e7dbcc6a4cf76
Marcio <marciohamer@gmail.com>
2023-05-11 22:38:32 -0300

    implementado layout e pagina de dash fixa

--- pen/app/admin/layout.tsx
+++ pen/app/admin/layout.tsx
@@ -1,6 +1,27 @@
-import AppBar from "app/AppBar";
+import Link from "next/link";
 import React from "react";
 
-export default function layout() {
-  return <div></div>;
+export default function layout({ children }: any) {
+  return (
+    <div>
+      <main className="container m-auto mt-4 px-4">
+        <div>
+          <ul>
+            <li className="content-center">
+              <Link href="/admin/dashboard" className="font-bold">
+                Dashboard
+              </Link>
+            </li>
+            <li>
+              <Link href="/admin/orders">Compras</Link>
+            </li>
+            <li>
+              <Link href="/admin/products">Produtos</Link>
+            </li>
+          </ul>
+        </div>
+      </main>
+      <div className="md:col-span-3">{children}</div>
+    </div>
+  );
 }
