9d4049d939f265d7ac156e35a0deb41f6fd34966
Marcio <marciohamer@gmail.com>
2023-05-11 21:30:49 -0300

    Ajustado appBar

--- pen/app/AppBar.tsx
+++ pen/app/AppBar.tsx
@@ -49,16 +49,6 @@ const AppBar = () => {
               </Menu.Button>
               <Menu.Items className="absolute right-0 w-56 origin-top-right bg-white  shadow-lg ">
                 <Menu.Item>
-                  <DropdownLink className="dropdown-link" href="/profile">
-                    Profile
-                  </DropdownLink>
-                </Menu.Item>
-                <Menu.Item>
-                  <DropdownLink className="dropdown-link" href="/order-history">
-                    Order History
-                  </DropdownLink>
-                </Menu.Item>
-                <Menu.Item>
                   <a
                     className="dropdown-link"
                     href="#"
